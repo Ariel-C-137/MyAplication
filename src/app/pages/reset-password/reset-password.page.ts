48284e68f9d15b41933ba724ef48abc0b330c3aa
Lumielulu <165588868+Lumielulu@users.noreply.github.com>
2024-09-06 17:21:06 -0400

    validaciones

--- src/app/pages/reset-password/reset-password.page.ts
+++ src/app/pages/reset-password/reset-password.page.ts
@@ -8,6 +8,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ResetPasswordPage implements OnInit {
   value!: string;
+  validationMessages: string [] = [];
+  validation = () => {
+    if(this.value.length < 9){
+      this.validationMessages.push("largo insuficiente" + this.value.length)
+    };
+    if(/[A-Z]/.test(this.value)!){
+      this.validationMessages.push("No contiene un caracter mayuscula")
+    }
+    if(/[0-9]/.test(this.value)!){
+      this.validationMessages.push("No contiene un numero")
+    }
+  }
+  validationResult = () => {
+    this.validation()
+    if(this.validationMessages.length > 1){
+      console.log(this.validationMessages)
+    }
+  }
   constructor() { }
 
   ngOnInit() {
--- src/app/pages/signup/signup.page.ts
+++ src/app/pages/signup/signup.page.ts
@@ -6,7 +6,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./signup.page.scss'],
 })
 export class SignupPage implements OnInit {
-
+  value!: string
+  validationMessages: string [] = [];
+  validation = () => {
+    if(this.value.length < 9){
+      this.validationMessages.push("largo insuficiente" + this.value.length)
+    };
+    if(/[A-Z]/.test(this.value)!){
+      this.validationMessages.push("No contiene un caracter mayuscula")
+    }
+    if(/[0-9]/.test(this.value)!){
+      this.validationMessages.push("No contiene un numero")
+    }
+  }
+  validationResult = () => {
+    this.validation()
+    if(this.validationMessages.length > 1){
+      console.log(this.validationMessages)
+    }
+  }
   constructor() { }
 
   ngOnInit() {
